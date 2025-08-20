5c3bcffd4ac4f7b15ff5f115afada56d50585be1
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-08-20 09:40:25 +0200

    Add plaatgames link

--- background.js
+++ background.js
@@ -39,8 +39,8 @@ function update(delta) {
         p.vy += rand(-0.05, 0.05);
         p.vx = Math.max(0.3, Math.min(p.vx, 2));
         p.vy = Math.max(-2, Math.min(p.vy, -0.3));
-        p.x += p.vx * delta * 0.05;
-        p.y += p.vy * delta * 0.05;
+        p.x += p.vx * 30 * delta;
+        p.y += p.vy * 30 * delta;
         if (p.x > window.innerWidth || p.y < -window.innerHeight) {
             Object.assign(p, createParticle(false));
         }
@@ -72,7 +72,7 @@ let oldTime = performance.now();
 function loop() {
     window.requestAnimationFrame(loop);
     let newTime = performance.now();
-    update(newTime - oldTime);
+    update((newTime - oldTime) / 1000);
     oldTime = newTime;
     draw();
 }
